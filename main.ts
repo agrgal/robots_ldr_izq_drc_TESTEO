4e7a7580e2835b7c152ab164c91e24427c510eed
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-22 21:09:57 +0200

    Estableciendo umbral en 200

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
+let umbral = 200
 basic.showIcon(IconNames.Confused)
 basic.pause(500)
 basic.forever(function () {
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     basic.pause(1000)
-    if (pins.analogReadPin(AnalogPin.P1) < 500) {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    } else {
+    if (pins.analogReadPin(AnalogPin.P1) < umbral) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    } else {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     }
 })
