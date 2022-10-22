f557665a6bef6c6e093d519bc1fc1d8157a01f22
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-22 21:06:01 +0200

    subsanar error

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 basic.showIcon(IconNames.Confused)
 basic.pause(500)
 basic.forever(function () {
-    basic.showNumber(pins.digitalReadPin(DigitalPin.P1))
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     basic.pause(1000)
-    if (pins.digitalReadPin(DigitalPin.P1) < 500) {
+    if (pins.analogReadPin(AnalogPin.P1) < 500) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     } else {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
